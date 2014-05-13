99dfde34a71f222b1c986d5ecf2e9eeac94895f1
daamdaam <macmillanmeister@gmail.com>
2014-05-13 13:43:55 +0100

    A really simple example of a HTML scraper
    This is done using Selenium and the InternetExplorerDriver and the
    PhantomJSDriver

---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>A Simple HTML Scraper using IE and PhantomJS</title>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.DoTheScrape').click(function () {
+                $('.ScrapedHtml').val(""); //clear the text area
+            })
+        })
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div class="main-content">
+        <h1>A really simple HTML Scraper written using Csharp and using PhantomJSDriver</h1>
+        <label for="txtUrlToScrape">Enter a URL to scrape - not one that requires logging in.</label>
+        <asp:TextBox Width="100%" ID="txtUrlToScrape" runat="server"/>
+        <asp:RadioButtonList runat="server" ID="radlstBrowserToUse">
+            <asp:ListItem Value="PhantomJSDriver" Text="Use PhantomJS Headless browser"></asp:ListItem>
+            <asp:ListItem Value="InternetExplorerDriver" Text="Use Internet Explorer browser"></asp:ListItem>
+        </asp:RadioButtonList> 
+        <asp:Button runat="server" ID="btnDoTheScrape" Text="Do the scrape" class="DoTheScrape"/>
+        <label for="txtScrapedHtml">The scraped HTML will appear in the Textbox below.</label>
+        <asp:TextBox TextMode="MultiLine" ReadOnly="true" runat="server" CssClass="ScrapedHtml" ID="txtScrapedHtml" Width="100%" Height="800px"></asp:TextBox>
+    </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// use the selenium dlls (one for IE and one for js)
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // clear the ScrapedHtml Textbox
+        txtScrapedHtml.Text = "";
+
+        if (!IsPostBack)
+        {
+        }
+        if (IsPostBack)
+        {
+            if (radlstBrowserToUse.SelectedValue == "PhantomJSDriver")
+            {
+                var Driver = createHeadlessBrowserDriver();
+
+                Driver.Navigate().GoToUrl(txtUrlToScrape.Text);
+                txtScrapedHtml.Text = Driver.PageSource.ToString();
+            }
+            else if (radlstBrowserToUse.SelectedValue == "InternetExplorerDriver")
+            {
+                var Driver = createIEBrowserDriver(); 
+                Driver.Navigate().GoToUrl(txtUrlToScrape.Text);
+                txtScrapedHtml.Text = Driver.PageSource.ToString();
+            }
+
+        }
+
+    }
+
+    // this is the phantomjs driver - the headless one
+    public PhantomJSDriver createHeadlessBrowserDriver()
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+        //Phantom driver - no browser shown
+        var driver = new PhantomJSDriver(path + "drivers");
+
+        return driver;
+    }
+
+    public InternetExplorerDriver createIEBrowserDriver()
+    {
+        // get the domain
+        string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+        // open IE to login to the website
+        var options = new InternetExplorerOptions()
+        {
+            IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+        };
+        var driver = new InternetExplorerDriver(path + "drivers", options);
+
+        return driver;
+    }
+}
